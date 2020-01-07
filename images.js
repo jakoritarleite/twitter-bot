235c511167355005b706257e942773d5cd9aa29a
João Koritar <jakoritarleite@gmail.com>
2020-01-07 03:19:02 -0300

    Removed images.js

--- images.js
+++
@@ -1,9 +0,0 @@
-var 519064-nvidia-geforce-rtx-2080-ti-founders-edition.jpg = [
- 	{
-		created_at: 'Tue Jan 07 05:04:02 +0000 2020',
- 		id: 1214412354299072500,
- 		id_str: '1214412354299072512',
- 		url: 'https:',
- 	}
-];
-
