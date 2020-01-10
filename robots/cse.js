ea810957aa8d32af7176b417e645bbfddfa7d82b
João Koritar <jakoritarleite@gmail.com>
2020-01-10 15:54:46 -0300

    feat: make function to download all images, and save filenames in state

--- robots/cse.js
+++ robots/cse.js
@@ -1,14 +1,14 @@
 const state = require('./state.js')
-const google = require('googleapis').google
+const google = require('googleapis').google,
+      imageDownloader = require('image-downloader')
 const googleCustomSearchCredentials = require('../credentials/google-search.json')
 
 async function robot() {
     const search = state.load()
     const customSearch = google.customsearch('v1')
 
-    await fetchGoogleAndReturnImages(search.term)
-    await downloadAllImagesReturned(search.urls)
-
+    //await fetchGoogleAndReturnImages(search.term)
+    await downloadAllImagesReturned()
     state.save(search)
 
     async function fetchGoogleAndReturnImages(term) {
@@ -25,10 +25,52 @@ async function robot() {
         search.urls = imageUrls
     }
 
-    async function downloadAllImagesReturned(links) {
-        return 'NULL'
+    async function downloadAllImagesReturned() {
+        search.filenames = []
+        for (urlIndex = 0; urlIndex < search.urls.length; urlIndex++) {
+            try {
+                await downloadImage(search.urls[urlIndex])
+                search.filenames[urlIndex] = await getFilename(search.urls[urlIndex], urlIndex)
+                console.log('[!] Downloaded image ' + search.filenames[urlIndex])
+                //break
+            } catch (error) {
+                console.log('[!] ERROR: downloading image ' + search.filenames[urlIndex])
+                console.log(error)
+            }
+        }
     }
 
+    async function downloadImage(url) {
+         return imageDownloader.image({
+             url, url,
+             dest: './content/images/'
+         })
+    }
+
+    async function getFilename(url, index) {
+        //console.log(url)
+        var isSlash = false
+		var isExtension = false
+		var fName = 0
+		var iName = 0
+		var filename = ''
+		for (i = url.length; i > 0; i--) {
+			if ((url[i - 3] + url[i - 2] + url[i - 1] + url[i]) == '.jpg' && !isExtension) {
+				fName = i
+				isExtension = true
+			}
+
+			if (url[i] == '/' && !isSlash) {
+				iName = i
+				isSlash = true;
+			}
+		}
+
+		for (i = iName + 1; i < fName + 1; i++) {
+			filename += url[i]
+		}
+        return filename
+    }
 }
 
 module.exports = robot
